b637b29393bc8661d4b01d3c50cd5be48c2e2b0f
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-12-10 17:35:31 +0000

    fixed database and environtment issues for 2.0
    git-svn-id: http://openmoney.rubyforge.org/svn/rubycc/trunk@135 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
 
   config.action_controller.session = {
     :session_key => '_rubycc_session_id',
-    :secret      => 'squidbumpers'
+    :secret      => '849fb63a18e03638e8850b7eb104d331'
   }
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
