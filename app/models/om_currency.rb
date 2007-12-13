28957ab83da4978f93233637dbf712a8fa179aba
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-12-13 14:06:44 +0000

    in manage currencies you can now see the transaction history
    git-svn-id: http://openmoney.rubyforge.org/svn/rubycc/trunk@143 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- app/models/om_currency.rb
+++ app/models/om_currency.rb
@@ -7,6 +7,6 @@
 class OmCurrency < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user_id,:omrl
-  validates_uniqueness_of :omrl
+  validates_uniqueness_of :omrl, :scope => [:user_id]
   attr_protected :user_id
 end
