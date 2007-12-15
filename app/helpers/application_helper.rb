7526e6875cd923c0f2a65f222c0444c086dd30d7
zippy <zippy@f945c0c2-b2a2-4481-81d4-5ff5a83ded3a>
2007-12-15 04:41:26 +0000

    prettied up interface for creating new entities
    git-svn-id: http://openmoney.rubyforge.org/svn/rubycc/trunk@151 f945c0c2-b2a2-4481-81d4-5ff5a83ded3a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,29 +20,34 @@ module ApplicationHelper
     select_tag(html_field_name,options_for_select(c,selected))
   end
   
-  def make_entity_base_fields(entity_type,default_tag='steward')
+  def make_entity_base_fields(entity_type,default_omrl='',default_tag='steward')
     context_select = om_contexts_select(:parent_context,params[:parent_context])
     result = ""
-    power = <<-EOHTML
+    detailed = <<-EOHTML
   	  <b>Credential for parent context access:</b><br />
   	  ID: #{text_field_tag(:context_tag,params[:context_tag] || 'steward')} Password: #{password_field_tag :context_password,params[:context_password]}
   	<br />
-    <b>#{entity_type} omrl:</b> #{ text_field_tag :omrl,params[:omrl]}
+    <b>#{entity_type} omrl:</b> #{ text_field_tag :omrl,params[:omrl]||default_omrl}
     EOHTML
     if context_select
-      result << <<-EOHTML
+      name_and_context = <<-EOHTML
       <p id="power" style="display: none">
-      #{power}
+      #{detailed}
       </p>
       <p id="easy">
       <b>#{entity_type} Name: </b> #{ text_field_tag :name,params[:name] } in #{context_select}
       </p>
-      (<input id="power_user" type="checkbox" name="power_user" value="1" onClick="if($F('power_user')) {$('easy').hide();$('power').show()} else {$('easy').show();$('power').hide()}" #{params[:power_user] ? 'checked' : ''} > detailed)
+      
       EOHTML
+      power = %Q|(<input id="power_user" type="checkbox" name="power_user" value="1" onClick="if($F('power_user')) {$('easy').hide();$('power').show()} else {$('easy').show();$('power').hide()}" #{params[:power_user] ? 'checked' : ''} > detailed)|
     else
-      result << power << %Q|<input id="power_user" type="hidden" name="power_user" value="1">|
+      name_and_context = detailed
+      power = %Q|<input id="power_user" type="hidden" name="power_user" value="1">|
     end
-    result << <<-EOHTML
+     <<-EOHTML
+   	<fieldset style="display: inline">
+   		<legend>#{entity_type} Info #{power}</legend>
+   		#{name_and_context}
     <p>
       <b>Description:</b><br />
       #{ text_field_tag :description,params[:description] ,:size => 50 }
@@ -53,7 +58,7 @@ module ApplicationHelper
       ID: #{text_field_tag :tag,params[:tag] || default_tag }
       Password: #{password_field_tag :password,params[:password] }
     </p>
-
+    </fieldset>
     EOHTML
     
   end
